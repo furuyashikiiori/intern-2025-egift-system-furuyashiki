12002e9c490f98c944172728f8f8c2f052c00ec6
aoineco <masaya.kiyomi@giftee.co>
2025-07-09 13:19:14 +0900

    itemsテーブルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_09_041353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.text "description"
+    t.boolean "published", default: false, null: false
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
